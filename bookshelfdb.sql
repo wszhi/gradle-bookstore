4c17bcf04bc2e1cdf7f92249a425500eb256d973
wszhi <wszhi@users.noreply.github.com>
2016-03-01 21:50:05 +0800

    update sql

--- bookshelfdb.sql
+++ bookshelfdb.sql
@@ -24,13 +24,13 @@ CREATE TABLE `cart` (
 -- ----------------------------
 -- Records of cart
 -- ----------------------------
-INSERT INTO `cart` VALUES ('8a176804532d10fc01532d125cb90000', 'http://api.douban.com/v2/book/2361768', '21', 'we', '[丹麦]金·弗珀兹·艾克松/文,[瑞典]爱娃·艾瑞克松/图', 'http://img3.doubanio.com/mpic/s3446498.jpg', '23.00', '483.00', '爷爷变成了幽灵');
-INSERT INTO `cart` VALUES ('8a176804532d462101532d4d22f20002', 'http://api.douban.com/v2/book/1052990', '12', 'we', '曹文轩', 'http://img3.douban.com/mpic/s2652540.jpg', '18.00', '216.00', '草房子');
-INSERT INTO `cart` VALUES ('8a176804532d676501532d67ee760000', 'http://api.douban.com/v2/book/1004088', '2', 'we', '[瑞典] 阿斯特丽德·林格伦', 'http://img3.doubanio.com/mpic/s1290828.jpg', '17.80', '35.60', '长袜子皮皮');
-INSERT INTO `cart` VALUES ('8a176804533125030153313c91bf0000', 'http://api.douban.com/v2/book/1007914', '7', 'we', '[日] 黑柳彻子 著/岩崎千弘 图/', 'http://img3.douban.com/mpic/s1067911.jpg', '20.00', '140.00', '窗边的小豆豆');
-INSERT INTO `cart` VALUES ('8a1768045331485801533148da3d0000', 'http://api.douban.com/v2/book/1036274', '4', 'we', '[美] E.B.怀特/', 'https://img1.doubanio.com/mpic/s1120437.jpg', '17.00', '68.00', '夏洛的网');
-INSERT INTO `cart` VALUES ('8a17680453314ae8015331d826fb0000', 'http://api.douban.com/v2/book/4909055', '1', '', '[英]康拉德/', 'https://img1.doubanio.com/mpic/s5155133.jpg', '30.00', '30.00', '黑暗的心 吉姆爷');
-INSERT INTO `cart` VALUES ('8a1768045331dbe7015331ddc6b20000', 'http://api.douban.com/v2/book/1001136', '1', 'we', '[英] 詹姆斯·巴里/孙国双 图/', 'https://img3.doubanio.com/mpic/s1000715.jpg', '13.80', '13.80', '彼得·潘');
+INSERT INTO `cart` VALUES ('8a176804532d10fc01532d125cb90000', 'http://api.douban.com/v2/book/2361768', '21', 'lily', '[丹麦]金·弗珀兹·艾克松/文,[瑞典]爱娃·艾瑞克松/图', 'http://img3.doubanio.com/mpic/s3446498.jpg', '23.00', '483.00', '爷爷变成了幽灵');
+INSERT INTO `cart` VALUES ('8a176804532d462101532d4d22f20002', 'http://api.douban.com/v2/book/1052990', '12', 'lily', '曹文轩', 'http://img3.douban.com/mpic/s2652540.jpg', '18.00', '216.00', '草房子');
+INSERT INTO `cart` VALUES ('8a176804532d676501532d67ee760000', 'http://api.douban.com/v2/book/1004088', '2', 'lily', '[瑞典] 阿斯特丽德·林格伦', 'http://img3.doubanio.com/mpic/s1290828.jpg', '17.80', '35.60', '长袜子皮皮');
+INSERT INTO `cart` VALUES ('8a176804533125030153313c91bf0000', 'http://api.douban.com/v2/book/1007914', '7', 'lily', '[日] 黑柳彻子 著/岩崎千弘 图/', 'http://img3.douban.com/mpic/s1067911.jpg', '20.00', '140.00', '窗边的小豆豆');
+INSERT INTO `cart` VALUES ('8a1768045331485801533148da3d0000', 'http://api.douban.com/v2/book/1036274', '4', 'lily', '[美] E.B.怀特/', 'https://img1.doubanio.com/mpic/s1120437.jpg', '17.00', '68.00', '夏洛的网');
+INSERT INTO `cart` VALUES ('8a17680453314ae8015331d826fb0000', 'http://api.douban.com/v2/book/4909055', '1', 'lily', '[英]康拉德/', 'https://img1.doubanio.com/mpic/s5155133.jpg', '30.00', '30.00', '黑暗的心 吉姆爷');
+INSERT INTO `cart` VALUES ('8a1768045331dbe7015331ddc6b20000', 'http://api.douban.com/v2/book/1001136', '1', 'lily', '[英] 詹姆斯·巴里/孙国双 图/', 'https://img3.doubanio.com/mpic/s1000715.jpg', '13.80', '13.80', '彼得·潘');
 
 -- ----------------------------
 -- Table structure for `user`
